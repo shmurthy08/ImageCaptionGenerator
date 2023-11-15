596f229378bf5700bc09073c70202d1cb890bbb0
Shree Murthy <shmurthy@chapman.edu>
2023-11-14 19:34:15 -0800

    Encoder.py and preprocessing

--- README.md
+++ README.md
@@ -3,13 +3,32 @@
 ## Important Information
 
 a) Shree Murthy, Dylan Inafuku, Rahul Sura
+
 b) shmurthy@chapman.edu, dinafuku@chapman.edu, sura@chapman.edu
+
 c) 393-02
+
 d) Final Project - Image Caption Generator
 
 ## Replicating Findings
 
 Refer to the proposal pdf document to understand overarching architecture and approach. 
 
+## Important Prequisities
+
+Download the following python libraries:
+```
+pip install tensorflow
+
+**Keras is installed as part of this pip install
+``` 
+
+```
+pip install pickle
+```
+
+
 ## Important Links 
 - [Proposal](https://github.com/shmurthy08/ImageCaptionGenerator/blob/main/proposal.pdf)
+- [Flicker8k Kaggle Dataset](https://www.kaggle.com/datasets/adityajn105/flickr8k)
+- [Flicker Source Documentation](https://hockenmaier.cs.illinois.edu/8k-pictures.html)
---
+++ src/encoder.py
@@ -0,0 +1,59 @@
+import os
+from tensorflow.keras.applications import InceptionV3
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import GlobalAveragePooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing import image as kimage
+import pickle
+import numpy as np
+
+# Load in InceptionV3 Model
+# Documentation for InceptionV3: https://keras.io/api/applications/inceptionv3/
+
+base_model = InceptionV3(weights='imagenet', include_top=False, input_shape=(300,300,3))
+
+# Remove classification layers
+pooled_output = base_model.layers[-1].output
+encoded_features = GlobalAveragePooling2D()(pooled_output)
+
+# Create updated model w/o classification layers
+encoder = Model(inputs=base_model.input, outputs=encoded_features)
+
+# Freeze layers in base model
+for layer in base_model.layers:
+    layer.trainable = False # ensures these layers aren't retrained 
+    
+    
+# Preprocess images
+# Path to your dataset directory
+dataset_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'dataset', 'Flicker8k', 'Images')
+
+
+# Create a list of image file paths
+image_paths = []
+for root, dirs, files in os.walk(dataset_dir):
+    for file in files:
+        if file.endswith(".jpg"):
+            image_paths.append(os.path.join(root, file))
+
+
+#print(image_paths)
+print(len(image_paths))
+
+# Preprocess and extract features for each image
+features = []
+for img_path in image_paths:
+    img = kimage.load_img(img_path, target_size=(300, 300))
+    img_array = kimage.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array / 255.0  # Normalizing pixel values
+    img_features = encoder.predict(img_array)
+    features.append(img_features)
+
+# Concatenate extracted features into a single array
+features = np.concatenate(features, axis=0)
+
+
+# Save extracted features via pkl file
+with open('extracted_feats.pkl', 'wb') as f:
+    pickle.dump(features, f)
