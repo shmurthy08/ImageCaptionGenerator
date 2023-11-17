51cff3746253024c7c10364e36d59fa362293901
shmurthy08 <shmurthy@chapman.edu>
2023-11-16 20:17:17 -0800

    Decoder.py complete and model generates captions

--- src/decoder.py
+++ src/decoder.py
@@ -6,12 +6,18 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.utils import to_categorical, plot_model
 from tensorflow.keras.models import Model
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.layers import Embedding, LSTM, Dense, Input, Dropout, add
-from tensorflow.keras.layers import LayerNormalization
+from tensorflow.keras.layers import LayerNormalization, BatchNormalization
 import pickle
 import os
+from PIL import Image
+import matplotlib.pyplot as plt
 
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
+
+from nltk.translate.bleu_score import corpus_bleu
+
 
 
 # Load image features
@@ -21,7 +27,7 @@ with open('extracted_feats.pkl', 'rb') as f:
 
 # Load GloVe embeddings
 glove_embeddings = {}
-with open('./glove_embeddings/glove.6B.100d.txt', encoding='utf-8') as f:
+with open('./glove_embeddings/glove.6B.200d.txt', encoding='utf-8') as f:
     for line in f:
         values = line.split()
         word = values[0]
@@ -94,6 +100,10 @@ cleaning(map)
 print(map['1000268201_693b08cb0e'])
 # Create an empty list to store all captions
 all_captions = []
+
+# Pickle the map dictionary
+with open('map.pkl', 'wb') as f:
+    pickle.dump(map, f)
 
 # Iterate over the dictionary items
 for key in map: 
@@ -178,62 +188,130 @@ for key, caps in map.items():
 
 
 
-# Combining GloVe with our caption embeddings
-embedding_dim = len(glove_embeddings['a'])
-embed_matrix = np.zeros((vocab_size, embedding_dim))
+# # Combining GloVe with our caption embeddings
+# embedding_dim = len(glove_embeddings['a'])
+# embed_matrix = np.zeros((vocab_size, embedding_dim))
 
-for word, index in tokenizer.word_index.items():
-    embedding_vec = glove_embeddings.get(word)
-    if embedding_vec is not None:
-        embed_matrix[index] = embedding_vec
+# for word, index in tokenizer.word_index.items():
+#     embedding_vec = glove_embeddings.get(word)
+#     if embedding_vec is not None:
+#         embed_matrix[index] = embedding_vec
         
 
-print("Image Features Shape: ", len(image_features))
+# print("Image Features Shape: ", len(image_features))
 
-# Define encoder model
-inputs1 = Input(shape=(2048,))
-fe1 = Dropout(0.5)(inputs1)
-fe2 = Dense(256, activation='relu')(fe1)
+# # Define encoder model
+# inputs1 = Input(shape=(2048,))
+# fe1 = BatchNormalization()(inputs1)
+# fe2 = Dense(256, activation='relu')(fe1)
 
-# Seq feature layer
-inputs2 = Input(shape=(max_length,))
-se1 = Embedding(vocab_size, embedding_dim, mask_zero=True)(inputs2)
-se2 = Dropout(0.5)(se1)
-se3 = LSTM(256, return_sequences=True)(se2)
-norm = LayerNormalization()(se3)
-se4 = LSTM(256)(norm)
-norm = LayerNormalization()(se4)
+# # Seq feature layer
+# inputs2 = Input(shape=(max_length,))
+# se1 = Embedding(vocab_size, embedding_dim, mask_zero=True)(inputs2)
+# se2 = LayerNormalization()(se1)
+# se3 = LSTM(256, return_sequences=True)(se2)
+# norm = LayerNormalization()(se3)
+# se4 = LSTM(256)(norm)
+# norm2 = LayerNormalization()(se4)
 
-# Decoder model
-decoder1 = add([fe2, norm])
-decoder2 = Dense(256, activation='relu')(decoder1)
-outputs = Dense(vocab_size, activation='softmax')(decoder2)
+# # Decoder model
+# decoder1 = add([fe2, norm2])
+# norm3 = LayerNormalization()(decoder1)
+# decoder2 = Dense(256, activation='relu')(norm3)
+# outputs = Dense(vocab_size, activation='softmax')(decoder2)
 
-# Tie it together
-decoder_model = Model(inputs=[inputs1, inputs2], outputs=outputs)
-decoder_model.summary()
-plot_model(decoder_model, to_file='complete_arch.png', show_shapes=True)
+# # Tie it together
+# decoder_model = Model(inputs=[inputs1, inputs2], outputs=outputs)
+# decoder_model.summary()
+# plot_model(decoder_model, to_file='complete_arch.png', show_shapes=True)
 
-# Compile the model
-decoder_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-
-steps = len(train_cap)//128
-epochs = 100 
+# # Compile the model
+# decoder_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 
-for epoch in range(epochs):
-    print(f"Epoch {epoch + 1}/{epochs}")
-    try:
-        generator = data_generator(train, map, image_features, tokenizer, max_length, vocab_size)
-        decoder_model.fit(generator, epochs=1, steps_per_epoch=steps, verbose=1)
-    except StopIteration:
-        break
-    print()  # Add a newline after each epoch
+# steps = len(train_cap)//128
+# epochs = 35 
+# val_steps=len(test_cap)//128
+
+# for epoch in range(epochs):
+#     print(f"Epoch {epoch + 1}/{epochs}")
+#     try:
+#         generator = data_generator(train, map, image_features, tokenizer, max_length, vocab_size)
+#         decoder_model.fit(generator, epochs=1, steps_per_epoch=steps, verbose=1)
+#         decoder_model.save('model.h5')
+#         print(f"Saved model on epoch: {epoch +1}")
+#     except StopIteration:
+#         break
+#     print()  # Add a newline after each epoch
 
 
-Model.save(decoder_model, 'decoder_model.h5')
-print("Saved model to disk")
-with open('tokenizer.pkl', 'wb') as f:
-    pickle.dump(tokenizer, f)
-print("Saved tokenizer to disk")
+# print("Saved model to disk")
+# with open('tokenizer.pkl', 'wb') as f:
+#     pickle.dump(tokenizer, f)
+# print("Saved tokenizer to disk")
+
+
+
+model = load_model('model.h5')
+with open('tokenizer.pkl', 'rb') as f:
+    tokenizer = pickle.load(f)
+    
+    
+def word_for_id(integer, tokenizer):
+    for word, index in tokenizer.word_index.items():
+        if index == integer:
+            return word
+    return None
+
+
+
+def pred_caption(model, image, tokenizer, max_length):
+    input_txt = 'startseq'
+    for i in range(max_length):
+        #encode input
+        seq = tokenizer.texts_to_sequences([input_txt])[0]
+        seq = pad_sequences([seq], maxlen=max_length)
+        #predict next word
+        y_hat = model.predict([image, seq], verbose=0)
+        y_hat = np.argmax(y_hat)
+        word = word_for_id(y_hat, tokenizer)
+        if word is None:
+            break
+        
+        
+        
+        input_txt += ' ' + word
+        if word == 'endseq':
+            break 
+      
+    return input_txt
+
+
+# Let's use the test data and assess performance using BLEU Score
+# for k in test:
+#     #get caption
+#     all_caps = map[k]
+#     # predict caption
+#     y_pred = pred_caption(m, image_features[k], tokenizer, max_length)
+#     y_actual = [caption.split() for caption in all_caps]
+#     # calc BLEU score
+#     print(f"BLEU-1: {corpus_bleu(y_actual[0], y_pred, weights=(1.0,0,0))}")
+
+
+
+
+
+def gener_caption(image_name):
+    image_id = image_name.split('.')[0]
+    img_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'dataset', 'Flicker8k', 'Images', image_name)
+    img = Image.open(img_path)
+    captions = map[image_id]
+    for cap in captions:
+        print(cap)
+    y_pred = pred_caption(model, image_features[image_id], tokenizer, max_length)
+    
+    print(y_pred[9:-7])
+    # plt.imshow(img)
+    
+    
+gener_caption('3712008738_1e1fa728da.jpg')
